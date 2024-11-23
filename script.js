ae0584bb2d9ee2a30de35f757a8b0b384ab14d0f
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2024-11-24 01:00:41 +0400

    Added initial js code.

---
+++ script.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+        question: " Which is the largets animal in the world?",
+        answers: [
+            { text: "Shark", correct: false},
+            { text: "Blue whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "Girraffe", correct: false},
+        ]
+    }
+]
