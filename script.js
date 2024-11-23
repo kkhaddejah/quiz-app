7e05ec1627adbaa2b3c9d592353d7e1c086f7eb4
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2024-11-24 01:06:58 +0400

    Added questions in js code.

--- script.js
+++ script.js
@@ -7,5 +7,32 @@ const questions = [
             { text: "Elephant", correct: false},
             { text: "Girraffe", correct: false},
         ]
-    }
+    },
+    {
+        question: " Which is the smallest city in the world?",
+        answers: [
+            { text: "Vatican", correct: true},
+            { text: "Bhutan", correct: false},
+            { text: "Nepal", correct: false},
+            { text: "Sri Lanka", correct: false},
+        ]
+    },
+    {
+        question: " Which is the largets desert in the world?",
+        answers: [
+            { text: "Kalahari", correct: false},
+            { text: "Gobi", correct: false},
+            { text: "Sahara", correct: false},
+            { text: "Antarctica", correct: true},
+        ]
+    },
+    {
+        question: " Which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arctic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    },
 ]
