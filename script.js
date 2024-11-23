b0d4fbfa76c5a4d3a2b4408aa4417353b1a82f4e
Khadejah <136977286+kkhaddejah@users.noreply.github.com>
2024-11-24 01:26:41 +0400

    Added js code.

--- script.js
+++ script.js
@@ -36,3 +36,30 @@ const questions = [
         ]
     },
 ];
+
+const qustionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    qustionElement.innerHTML = questionNo + ". " + currentQuestionIndex.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
+}
